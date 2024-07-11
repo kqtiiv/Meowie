ac59aaa184857a8453de08e41de02bee26fa86d6
Katie Wan <katiewan.kw@gmail.com>
2024-07-11 22:22:35 +0100

    Fix CSS bug

--- login.php
+++ login.php
@@ -14,7 +14,7 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Meowie</title>
-    <link rel="icon" href="/public/images/cat_2.png" >
+    <link href="/public/style.css" rel="stylesheet" />
     <link rel="icon" href="/public/images/cat_2.png" type="image/x-icon" >
     <link href="https://fonts.cdnfonts.com/css/public-pixel" rel="stylesheet" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
