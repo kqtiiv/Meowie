5f4bec546cbcca8606bd406ccabadd3e0ea3fb98
Katie Wan <katiewan.kw@gmail.com>
2024-07-22 21:56:58 +0100

    Fix redirect button

--- about.php
+++ about.php
@@ -57,8 +57,9 @@
             <h2>The developer</h2>
             <p>
               My name is Katie - also known as kqtiiv. I am a computer science student, who enjoys web development, game development, 
-              cryptography and theoretical computing. My goal is to study CS at University and work in the sector in the future. 
-              Evidently, I also like cats, and they would greatly appreciate any form of donation to aid in web hosting and game hosting.
+              cryptography and theoretical computing. My current goal is to study CS at University, so I can further expand and develop 
+              my knowledge for the subject through lectures and independent research. 
+              Evidently, I also like cats, and they would greatly appreciate any form of donation to aid in web hosting.
             </p>
             <h2>I want more!</h2>
             <p>
@@ -88,7 +89,7 @@
                 </svg>
               </a>
               <a href="www.linkedin.com/in/kqtiiv">
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-linkedin" viewBox="0 0 16 16">
+                <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-linkedin" viewBox="0 0 16 16">
                   <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854zm4.943 12.248V6.169H2.542v7.225zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248S2.4 3.226 2.4 3.934c0 .694.521 1.248 1.327 1.248zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016l.016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225z"/>
                 </svg>
               </a>
@@ -98,7 +99,7 @@
             <h2>Donate</h2>
             <p>Meowie thanks you for your kindness!</p>
             <form action="https://buymeacoffee.com/kqtiiv" >
-                <input class="btn" type="submit" value="Redirect me to donate page" />
+                <input class="btn" type="submit" value="Redirect" />
             </form>
        </div>
       </div>
